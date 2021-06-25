99701153630a72999f0dc17a3f1b3a05d953da6c
EverlastingHolder <moshkovcevroma@gmail.com>
2021-06-25 14:18:54 +0700

    Initial Commit

---
+++ BookPlay/BookPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPlayApp.swift
+//  BookPlay
+//
+//  Created by Роман Мошковцев on 25.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
